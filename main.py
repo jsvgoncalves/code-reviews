1e1b08f3d0dab7f280814fa8960cfd3cffecbe36
Antonio Silva <silva@ecoinvent.org>
2023-08-09 11:04:10 +0200

    Add query param to include important people

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ from fastapi import FastAPI, Body, HTTPException, status
 app = FastAPI()
 
 
+IMPORTANT_PEOPLE = ["Boss", "Ned Stark", "Robert Baratheon", "Jaime Lannister"]
+
+
+def add_to_destination_list(values: list[str], default=["Boss"]):
+    to = default
+    for value in values:
+        if value not in default:
+            to.append(value)
+    return to
+
+
 @app.post("/generate_message")
 async def generate_message(
     destination: list[str] = Body(),
     message: str = Body("Hello World"),
+    include_important_people: bool = False,
 ):
     """
     Generate a message that will be sent to all the people in the destination list.
     - **destination**: A list of people that should receive the message.
     - **message**: The content of the message that will be sent.
+    - **include_important_people**: Whether to include important people in the destination list, default `False`.
     """
-    return {"message": message, "destination": ["Boss"] + destination}
-
+    if include_important_people:
+        destination = add_to_destination_list(destination, default=IMPORTANT_PEOPLE)
+    else:
+        destination = add_to_destination_list(destination)
+ 
+    return {"message": message, "destination": destination}
